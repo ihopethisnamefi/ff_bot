a1de3749339b2a8804745ee732bce22b4497b9e8
ihopethisnamefi <jonthibeault@gmail.com>
2017-09-20 13:42:41 -0500

    formatting
    formatting

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -129,7 +129,7 @@ def bot_main(function):
     elif function=="get_power_rankings":
         text = get_power_rankings(league)
         bot.send_message(text)
-	elif function=="get_test":
+    elif function=="get_test":
         text = get_test(league)
         bot.send_message(text)
     elif function=="init":
@@ -179,6 +179,6 @@ if __name__ == '__main__':
     sched.add_job(bot_main, 'cron', ['get_close_scores'], id='close_scores', day_of_week='mon', hour=18, minute=30, start_date=ff_start_date, end_date=ff_end_date, timezone=myTimezone, replace_existing=True)
     sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard1', day_of_week='fri,mon,tue', hour=7, minute=30, start_date=ff_start_date, end_date=ff_end_date, timezone=myTimezone, replace_existing=True)
     sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard2', day_of_week='sun', hour='16,20', start_date=ff_start_date, end_date=ff_end_date, timezone=myTimezone, replace_existing=True)
-	sched.add_job(bot_main, 'cron', ['get_test'], id='test', day_of_week='wed', minute='0-59', start_date=ff_start_date, end_date=ff_end_date, timezone=myTimezone, replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['get_test'], id='test', day_of_week='wed', minute='0-59', start_date=ff_start_date, end_date=ff_end_date, timezone=myTimezone, replace_existing=True)
 
     sched.start()
